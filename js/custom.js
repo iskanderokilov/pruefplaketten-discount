596816a4c53726f629491519cc8652c34a9ee5df
iskanderokilov <iskanderokilov@users.noreply.github.com>
2019-06-20 08:41:44 +0200

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -1,15 +1,11 @@
-const countCssColours = 5; // Количество вариаций цвета
+var countCssColours = 5; // Количество вариаций цвета
+var h = document.querySelector('.hero-caption p');
 
-const h = document.querySelector('.hero-caption p');
-// Создаём элементы для каждой буквы
-let spans = [];
-h.innerText.split('').forEach(ch => {
-  const el = document.createElement('span');
-  el.innerText = ch;
-  const colorIndex = Math.floor(Math.random() * countCssColours + 1);
-  el.classList.add(`color-${colorIndex}`);
-  spans.push(el);
-})
+var spans = "";
+h.innerText.split('').forEach(function (ch) {
+  var colorIndex = Math.floor(Math.random() * countCssColours + 1);
+  spans += '<span class="color-'+colorIndex+'">'+ch+'</span>';
+});
+
 // Добавляем элементы
-h.innerHTML = '';
-h.append(...spans);
+h.innerHTML = spans;
