945cd489e5ad4f93203629ade953d2a8e792e53d
Alexander Naumov <a.naumov@labelident.de>
2019-07-29 10:27:34 +0200

    update files styles and js for sticky header and navigation

--- js/custom.js
+++ js/custom.js
@@ -1,11 +1,30 @@
-var countCssColours = 5; // Количество вариаций цвета
-var h = document.querySelector('.hero-caption p');
+const countCssColours = 5; // Количество вариаций цвета
 
-var spans = "";
-h.innerText.split('').forEach(function (ch) {
-  var colorIndex = Math.floor(Math.random() * countCssColours + 1);
-  spans += '<span class="color-'+colorIndex+'">'+ch+'</span>';
-});
-
+const h = document.querySelector('.hero-caption p');
+// Создаём элементы для каждой буквы
+let spans = [];
+h.innerText.split('').forEach(ch => {
+  const el = document.createElement('span');
+  el.innerText = ch;
+  const colorIndex = Math.floor(Math.random() * countCssColours + 1);
+  el.classList.add(`color-${colorIndex}`);
+  spans.push(el);
+})
 // Добавляем элементы
-h.innerHTML = spans;
+h.innerHTML = '';
+h.append(...spans);
+
+// sticky nav und header scripts
+ window.onscroll = function() {fixTopElements()};
+      var header = document.getElementById("fix-on-scroll-header");
+      var nav = document.getElementById("navigation");
+      
+      function fixTopElements() {
+        if (window.pageYOffset > 33) { // 33px ist die Höhe vom header-top - Leiste
+          header.classList.add("sticky-header");
+          nav.classList.add("sticky-nav");
+          } else {
+          header.classList.remove("sticky-header");
+          nav.classList.remove("sticky-nav");
+        }
+      }
